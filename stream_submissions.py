5e0bf95a6c7dfede63448d652ac8b47a4d932075
bill <williamtwynn@gmail.com>
2019-01-26 15:54:10 -0800

    Formatted with black

--- stream_submissions.py
+++ stream_submissions.py
@@ -7,16 +7,18 @@ import vars
 
 
 # Instantiate a reddit object
-reddit = praw.Reddit(client_id=vars.client_id,
-                     client_secret=vars.client_secret,
-                     user_agent=vars.user_agent)
+reddit = praw.Reddit(
+    client_id=vars.client_id,
+    client_secret=vars.client_secret,
+    user_agent=vars.user_agent,
+)
 
 # Instantiate a kinesis client
-kinesis = boto3.client('kinesis')
+kinesis = boto3.client("kinesis")
 
 list_target = []
 count = 0
-submissions = reddit.subreddit('all').stream.submissions()
+submissions = reddit.subreddit("all").stream.submissions()
 
 
 try:
@@ -24,27 +26,29 @@ try:
         try:
             for s in submissions:
 
-                jsonItem = json.dumps({"title": s.title,
-                                       "sub": s.subreddit_name_prefixed,
-                                       "author": s.author.fullname,
-                                       "nsfw": s.over_18})
+                jsonItem = json.dumps(
+                    {
+                        "title": s.title,
+                        "sub": s.subreddit_name_prefixed,
+                        "author": s.author.fullname,
+                        "nsfw": s.over_18,
+                    }
+                )
 
-                list_target.append({"Data": jsonItem,
-                                    "PartitionKey": "filler"})
+                list_target.append({"Data": jsonItem, "PartitionKey": "filler"})
                 count += 1
                 print(count)
                 if count == 50:
-                    kinesis.put_records(StreamName='reddit',
-                                        Records=list_target)
-                    print('----------')
-                    print('SENDING RECORDS TO KINESIS')
-                    print('----------')
+                    kinesis.put_records(StreamName="reddit", Records=list_target)
+                    print("----------")
+                    print("SENDING RECORDS TO KINESIS")
+                    print("----------")
                     count = 0
                     list_target = []
 
         except Exception as e:
             print()
-            print(f'Exception: {e}')
+            print(f"Exception: {e}")
             print()
 
 except KeyboardInterrupt:
